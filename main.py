7f5dae73ac5809a3981bfa2c6bb565cdb93bf4fc
widejoy <rogerantony40@ug.cusat.ac.in>
2023-04-16 21:38:25 +0530

    debugged

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ image_2 = face_recognition.load_image_file(string_1)
 image_1_loc = face_recognition.face_locations(image_1)
 image_2_loc = face_recognition.face_locations(image_2)
 
-for j in range(len(image_2_loc)):
+for j in range(len(image_1_loc)):
  encoding_image_1 = face_recognition.face_encodings(image_1)[j]
- for i in range(len(image_1_loc)):
+ for i in range(len(image_2_loc)):
   encoding_image_2 = face_recognition.face_encodings(image_2)[i]
   results = face_recognition.compare_faces([encoding_image_1],encoding_image_2,tolerance=0.5)
   if results[0]:
@@ -70,19 +70,25 @@ frm = ttk.Frame(root, padding=50)
 frm.grid()
 ttk.Label(frm, text=a).grid(column=0, row=0)
 root.mainloop()
-top_1 = image_1_loc[i][0]
-right_1 = image_1_loc[i][1]
-bottom_1 = image_1_loc[i][2]
-left_1 = image_1_loc[i][3]
+
+
+top_1 = image_2_loc[i][0]
+right_1 = image_2_loc[i][1]
+bottom_1 = image_2_loc[i][2]
+left_1 = image_2_loc[i][3]
+
+
 top = image_1_loc[j][0]
 right = image_1_loc[j][1]
 bottom = image_1_loc[j][2]
 left = image_1_loc[j][3]
+
+
 image_1_pil = Image.fromarray(image_1)
 image_2_pil = Image.fromarray(image_2)
 draw = ImageDraw.Draw(image_1_pil)
-draw.rectangle((left_1,top_1,right_1,bottom_1),outline=(250,7,0))
+draw.rectangle((left,top,right,bottom),outline=(250,7,0))
 draw_1 = ImageDraw.Draw(image_2_pil)
-draw_1.rectangle((left,top,right,bottom),outline=(250,7,0))
+draw_1.rectangle((left_1,top_1,right_1,bottom_1),outline=(250,7,0))
 image_1_pil.show()
 image_2_pil.show()
